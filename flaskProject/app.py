1256a2000e6d176c545f188581fb2d0d3dd407d9
plutoissaplanet <epolanek@gmail.com>
2024-04-29 14:35:11 +0200

    geniuenly forgot what i last commited sooo yeah

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,9 +1,11 @@
+import random
+import base64
+from io import BytesIO
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
-from sqlalchemy import delete
-import datetime
-from sqlalchemy import DateTime
+from matplotlib.figure import Figure
+import numpy as np
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mindenszipiszuper.db'
@@ -49,6 +51,93 @@ class ListOfSubs(db.Model):
     logo_url = db.Column(db.String)
 
 
+class Graphs:
+    def __init__(self, x, y, label):
+        self._x = x
+        self._y = y
+        self._label = label
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, x):
+        self._x = x
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, y):
+        self._y = y
+
+    @property
+    def label(self):
+        return self._label
+
+    @label.setter
+    def label(self, label):
+        self._label = label
+
+    def coloring(self, param):
+        colors = ["#00897B", "#32A27F", "#5EBA7D", "#8CD178", "#C0E673", "#F9F871",
+                  "#746BBB", "#A558A1"]
+        nr_needed = len(param)
+        selected_colors = random.sample(colors, k=nr_needed)
+        return selected_colors
+
+    def customize_axes(self, ax):
+        ax.tick_params(axis='x', colors='white')
+        ax.tick_params(axis='y', colors='white')
+        ax.spines[['right', 'top']].set_visible(False)
+        ax.spines['bottom'].set_color('white')
+        ax.spines['left'].set_color('white')
+
+
+class BarChart(Graphs):
+    def __init__(self, x, y, label):
+        super().__init__(x, y, label)
+
+    def set_data(self, x, y):
+        self.x = x
+        self.y = y
+
+    def set_label(self, label):
+        self.label = label
+
+    def plot_graphs(self):
+        fig = Figure()
+        ax = fig.subplots()
+        ax.bar(self.x, self.y, color=self.coloring(self.x))
+        self.customize_axes(ax)
+        buf = BytesIO()
+        fig.savefig(buf, format="png", transparent=True)
+        data = base64.b64encode(buf.getvalue()).decode("ascii")
+        return data
+
+
+class PieChart(Graphs):
+    def __init__(self, x, y, label):
+        super().__init__(x, y, label)
+
+    def plot_graphs(self):
+        fig = Figure()
+
+        self.y = np.array(self.y).flatten()
+        ax = fig.subplots()
+        ax.pie(self.y, labels=self.label, colors=self.coloring(self.y),
+               wedgeprops=dict(width=0.3, edgecolor='w'),
+               textprops={'color': "w"},
+               autopct=lambda pct: f"({int(pct / 100 * sum(self.y))})")
+
+        buf = BytesIO()
+        fig.savefig(buf, format="png", transparent=True)
+        data = base64.b64encode(buf.getvalue()).decode("ascii")
+        return data
+
+
 with app.app_context():
     db.create_all()
 
@@ -66,13 +155,40 @@ def main_page():
 
 @app.route('/stats_page')
 def stats_page():
-    return render_template('statistics.html')
+    usersubs = Subs.query.filter_by(user_id=current_user.id, valuta="HUF").all()
+    counted_categories = count_subs(usersubs)
+    graph_bars = BarChart(x=["Gaming", "Newspaper", "Streaming", "Music"],
+                          y=counted_categories, label="")
+    graph_pie = PieChart(y=[counted_categories],
+                         label=["Gaming", "Newspaper", "Streaming", "Music"], x=0)
+    return render_template('statistics.html', usersubs=usersubs, plot_url=graph_bars.plot_graphs(),
+                           plot_url2=graph_pie.plot_graphs())
+
+
+def count_subs(subslist):
+    nr_gaming = 0
+    nr_newspaper = 0
+    nr_streaming = 0
+    nr_music = 0
+    category = []
+    counted_categories = []
+    for subs in subslist:
+        category.append(subs.category)
+        nr_gaming = category.count("Gaming")
+        nr_newspaper = category.count("Newspaper")
+        nr_streaming = category.count("Streaming")
+        nr_music = category.count("Music")
+    counted_categories.append(nr_gaming)
+    counted_categories.append(nr_newspaper)
+    counted_categories.append(nr_streaming)
+    counted_categories.append(nr_music)
+    return counted_categories
 
 
 @app.route('/input_page')
 def input_page():
     listofsubs = ListOfSubs.query.all()
-    usersubs = User.query.all()
+    usersubs = Subs.query.filter_by(user_id=current_user.id).all()
     return render_template('input.html', listofsubs=listofsubs, usersubs=usersubs)
 
 
@@ -109,7 +225,8 @@ def register_user():
     return render_template("registration_page.html")
 
 
-# login kódrészlet forrás: https://www.geeksforgeeks.org/how-to-add-authentication-to-your-app-with-flask-login/
+# login kódrészlet forrás:
+# https://www.geeksforgeeks.org/how-to-add-authentication-to-your-app-with-flask-login/
 # https://flask-sqlalchemy.palletsprojects.com/en/3.1.x/quickstart/#define-models
 
 @login_manager.user_loader
@@ -142,8 +259,6 @@ def logout():
 
 @app.route("/addnewsubscription", methods=["GET", "POST"])
 def add_new_subscription():
-    usersubs = User.query.all()
-
     if request.method == "POST":
         sub_name = request.form.get('subname')
         if not sub_name:
@@ -163,6 +278,7 @@ def add_new_subscription():
         sub_type = request.form.get('subtype')
 
         existingsub = ListOfSubs.query.filter_by(name=sub_name).one_or_none()
+
         if existingsub is None:
             new_sub = ListOfSubs(
                 name=sub_name,
@@ -181,6 +297,8 @@ def add_new_subscription():
         add(new_sub)
         db.session.commit()
         flash("Subscription added successfully!")
+
+    usersubs = Subs.query.filter_by(user_id=current_user.id).all()
 
     return render_template("input.html", usersubs=usersubs)
 
--- flaskProject/subs_db_init.py
+++ flaskProject/subs_db_init.py
@@ -1,4 +1,4 @@
-from app import db, ListOfSubs, app, delete, User
+from app import db, ListOfSubs, app,  User, Subs
 
 netflix = ListOfSubs(name='Netflix', logo_url="static/Photos/SubsIcons/netflix.png")
 spotify = ListOfSubs(name="Spotify", logo_url="static/Photos/SubsIcons/spotify.png")
@@ -13,14 +13,18 @@ def check_if_exists(data):
 
 
 with app.app_context():
-    usersubs = User.query.all()
-    for i in usersubs:
-        for j in i.subscriptions:
-            print("user subs: ", j.name)
+    usersubs = Subs.query.filter_by(user_id=1).all()
+    nr_gaming =0
+    category =[]
+    for subs in usersubs:
+        category.append(subs.category)
+        nr_gaming=category.count("Gaming")
+    print(category)
+    print(nr_gaming)
 
-    subs=ListOfSubs.query.all()
-    for i in subs:
-        print(i.name)
+    # subs=ListOfSubs.query.all()
+    # for i in subs:
+    #     print(i.name)
 
 
     if not check_if_exists(netflix):
